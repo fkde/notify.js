98cb763ca28e7dec1925fd561a46b6af138cf17c
FloKnapp <office@florianknapp.de>
2024-04-19 21:49:25 +0200

    Implement translation with variables in translation key
    Signed-off-by: FloKnapp <office@florianknapp.de>

--- src/Translator.mjs
+++ src/Translator.mjs
@@ -3,15 +3,48 @@ import en from './translation/en.json' with {type: 'json'};
 
 class Translator {
 
+    #translations = {
+        'de': de,
+        'en': en
+    };
+
+    constructor(lang) {
+        this.lang = lang;
+    }
+
     translate(slug) {
-        return de[slug] || en[slug] || slug;
+
+        const languageObj = this.#translations[this.lang];
+
+        if (languageObj[slug]) {
+            return languageObj[slug];
+        }
+
+        for (const key of Object.keys(languageObj)) {
+
+            const regex = key.replaceAll(/(\${\w+})/g, '(\\w+)');
+            const match = slug.match(regex);
+
+            if (match) {
+
+                let keyString = languageObj[key];
+
+                match.values().forEach((item) => {
+                    if (item === slug) return;
+                    keyString = keyString.replace(/(\${\w+})/, item);
+                });
+
+                return keyString;
+            }
+
+        }
     }
 
 }
 
-export default function translate(key) {
+export default function translate(key, lang = 'de') {
 
-    const translator = new Translator();
+    const translator = new Translator(lang);
     return translator.translate(key);
 
 };
